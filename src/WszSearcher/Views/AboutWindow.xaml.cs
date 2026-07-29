a40eda7962e536cb99d92a6ba195bb4aac82dbea
wszhoho <9058307@qq.com>
2026-07-29 13:34:32 +0800

    关于对话框修复版本号硬编码，读取实际版本号。

--- src/WszSearcher/Views/AboutWindow.xaml.cs
+++ src/WszSearcher/Views/AboutWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Windows;
 
 namespace WszSearcher.Views;
@@ -7,6 +8,8 @@ public partial class AboutWindow : Window
     public AboutWindow()
     {
         InitializeComponent();
+        var ver = Assembly.GetEntryAssembly()?.GetName()?.Version;
+        VersionText.Text = ver is not null ? $"v{ver.Major}.{ver.Minor}.{ver.Build}" : "v1.0.0";
     }
 
     private void OnCloseClick(object sender, RoutedEventArgs e)
